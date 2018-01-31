2106e69ffaf286d793ca6cf7d68cc5f205cb4407
Santos Gallegos <santos_g@outlook.com>
2018-01-31 04:05:36 -0500

    Gather repositories asynchronous

--- rplugin/python3/deoplete/sources/deoplete_plugin.py
+++ rplugin/python3/deoplete/sources/deoplete_plugin.py
@@ -1,22 +1,12 @@
 import json
 import re
 from urllib import request
+from urllib.error import HTTPError
 
 from .base import Base
 
 
-def get_repos(user):
-    base_url = 'https://api.github.com/users/{user}/repos'.format(
-        user=user
-    )
-    with request.urlopen(base_url) as r:
-        raw_response = r.read().decode('utf-8')
-        json_response = json.loads(raw_response)
-        return [
-            repo['name']
-            for repo in json_response
-        ]
-    return []
+REPOS_PER_PAGE = 20
 
 
 class Source(Base):
@@ -28,13 +18,50 @@ class Source(Base):
         self.mark = '[P]'
         self.filetypes = ['vim']
         self.input_pattern = r'(Plug)\s+\'(.+)/'
+        self._current_page = 1
 
     def get_complete_position(self, context):
         m = re.search(self.input_pattern, context['input'])
         return m.end() if m else -1
 
+    def _get_repo_endpoint(self, user, page):
+        return (
+            'https://api.github.com/users/{user}/repos'
+            '?page={page}&per_page={per_page}'.format(
+                user=user,
+                page=page,
+                per_page=REPOS_PER_PAGE
+            )
+        )
+
+    def _get_repos(self, user, page):
+        base_url = self._get_repo_endpoint(user, page)
+        try:
+            r = request.urlopen(base_url)
+            raw_response = r.read().decode('utf-8')
+            json_response = json.loads(raw_response)
+            return [
+                {
+                    'word': repo['name'],
+                    'menu': repo['description'],
+                }
+                for repo in json_response
+            ]
+        except HTTPError as e:
+            if e.code == 403:
+                self.print('API rate limit exceeded')
+        except Exception as e:
+            pass
+        return []
+
+    def _get_user(self, text):
+        m = re.search(self.input_pattern, text)
+        return m.group(2)
+
     def gather_candidates(self, context):
         input_text = context['input']
-        m = re.search(self.input_pattern, input_text)
-        user = m.group(2)
-        return get_repos(user)
+        user = self._get_user(input_text)
+        repos = self._get_repos(user, self._current_page)
+        self._current_page += 1
+        context['is_async'] = bool(repos)
+        return repos
